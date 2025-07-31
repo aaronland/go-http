038c77323efa28ce801b907c0cb98a29429c0ea6
thisisaaronland <thisisaaronland@localhost>
2025-07-31 11:31:23 -0700

    cli tools

---
+++ cmd/crumb/encrypted/main.go
@@ -0,0 +1,29 @@
+// encrypted returns a new URI suitable for instantiating an `EncryptedCrumb` instance.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/aaronland/go-http/v3/crumb"	
+)
+
+func main() {
+
+	ttl := flag.Int("ttl", 3600, "Time to live (in seconds)")
+	key := flag.String("key", "", "Optional key to use when generating crumb base")
+
+	flag.Parse()
+
+	ctx := context.Background()
+
+	uri, err := crumb.NewRandomEncryptedCrumbURI(ctx, *ttl, *key)
+
+	if err != nil {
+		log.Fatalf("Failed to generate crumb URI, %v", err)
+	}
+
+	fmt.Println(uri)
+}
---
+++ cmd/fileserver/fileserver/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/aaronland/go-http/v3/fileserver"
+	"github.com/aaronland/go-http/v3/server"
+	"github.com/sfomuseum/go-flags/multi"
+)
+
+func main() {
+
+	schemes := server.Schemes()
+	str_schemes := strings.Join(schemes, ", ")
+
+	server_desc := fmt.Sprintf("A valid aaronland/go-http-server URI. Registered schemes are: %s.", str_schemes)
+
+	server_uri := flag.String("server-uri", "http://localhost:8080", server_desc)
+	root := flag.String("root", "", "A valid path to serve files from.")
+	prefix := flag.String("prefix", "", "A prefix to append to URL to serve requests from.")
+
+	enable_cors := flag.Bool("enable-cors", false, "Enable CORS headers on responses.")
+	enable_gzip := flag.Bool("enable-gzip", false, "Enable gzip-ed responses.")
+
+	cors_origins := flag.String("cors-origins", "*", "A comma-separated of origins to allow CORS requests from.")
+
+	var mimetypes multi.KeyValueString
+	flag.Var(&mimetypes, "mimetype", "One or more key=value pairs mapping a file extension to a specific content (or mime) type to assign for that request")
+
+	var header_flags multi.KeyValueString
+	flag.Var(&header_flags, "header", "Zero or more key=value pairs to assign to the response headers for requests.")
+
+	flag.Parse()
+
+	ctx := context.Background()
+
+	s, err := server.NewServer(ctx, *server_uri)
+
+	if err != nil {
+		log.Fatalf("Unable to create server (%s), %v", *server_uri, err)
+	}
+
+	if *root == "" {
+
+		cwd, err := os.Getwd()
+
+		if err != nil {
+			log.Fatalf("Failed to derive current working directory, %v", err)
+		}
+
+		*root = cwd
+	}
+
+	fs_opts := &fileserver.FileServerOptions{
+		Root:        *root,
+		EnableCORS:  *enable_cors,
+		CORSOrigins: strings.Split(*cors_origins, ","),
+		EnableGzip:  *enable_gzip,
+	}
+
+	fs_handler, err := fileserver.NewFileServerHandler(fs_opts)
+
+	if err != nil {
+		log.Fatalf("Unable to create fileserver handler, %v", err)
+	}
+
+	uri := "/"
+
+	if *prefix != "" {
+
+		if !strings.HasPrefix(*prefix, "/") {
+			log.Fatalf("Invalid prefix. Missing leading /")
+		}
+
+		fs_handler = http.StripPrefix(*prefix, fs_handler)
+		uri = filepath.Join(uri, *prefix)
+	}
+
+	if !strings.HasSuffix(uri, "/") {
+		uri = fmt.Sprintf("%s/", uri)
+	}
+
+	root_handler := fs_handler
+
+	//
+
+	if len(mimetypes) > 0 {
+
+		matches := make(map[string]string)
+
+		for _, kv := range mimetypes {
+
+			matches[kv.Key()] = kv.Value().(string)
+		}
+
+		ct_opts := &fileserver.ContentTypeOptions{
+			Matches: matches,
+		}
+
+		h, err := fileserver.NewContentTypeHandler(ct_opts, root_handler)
+
+		if err != nil {
+			log.Fatalf("Failed to create new content type handler, %v", err)
+		}
+
+		root_handler = h
+	}
+
+	if len(header_flags) > 0 {
+
+		headers := make(map[string]string)
+
+		for _, kv := range header_flags {
+			headers[kv.Key()] = kv.Value().(string)
+		}
+
+		h, err := fileserver.NewWithHeadersHandler(headers, root_handler)
+
+		if err != nil {
+			log.Fatalf("Failed to create new content type handler, %v", err)
+		}
+
+		root_handler = h
+
+	}
+
+	//
+
+	mux := http.NewServeMux()
+	mux.Handle(uri, root_handler)
+
+	log.Printf("Serving %s and listening for requests on %s", *root, s.Address())
+
+	err = s.ListenAndServe(ctx, mux)
+
+	if err != nil {
+		log.Fatalf("Failed to start server, %v", err)
+	}
+}
